cb38ce2e08553d68fbea5498e2aa014dc98c1815
Jonathanldk <43172344+Jonathanldk@users.noreply.github.com>
2019-03-17 17:54:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HomeExerciseThree
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User");
+
+            while (true)
+            {
+                Console.WriteLine("Write down one number");
+
+                string input1 = Console.ReadLine();
+
+           
+                Console.WriteLine("Write down a second number");
+
+                string input2 = Console.ReadLine();
+
+
+                Console.WriteLine("Write down a third number");
+            
+                string input3 = Console.ReadLine();
+
+
+                Stack mystack = new Stack();
+                mystack.Push(input1);
+                mystack.Push(input2);
+                mystack.Push(input3);
+
+                foreach (Object obj in mystack)
+                {
+                    Console.WriteLine(obj);
+                }
+            }  
+        }
+    }
+}
